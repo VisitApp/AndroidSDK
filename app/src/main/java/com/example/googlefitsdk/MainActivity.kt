6481ba56d18dc5c0da618405d08c93f1be41d20f
Chinmay <chinmaydash11@gmail.com>
2021-12-22 21:21:00 +0530

    removed code.

--- app/src/main/java/com/example/googlefitsdk/MainActivity.kt
+++ app/src/main/java/com/example/googlefitsdk/MainActivity.kt
@@ -24,12 +24,8 @@ class MainActivity : AppCompatActivity() {
 
         val default_client_id =
             "74319562719-7rart63dq265045vtanlni9m8o41tn7o.apps.googleusercontent.com"
-        val magicLink =
-            "https://star-health.getvisitapp.xyz/star-health?token=eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOi[%E2%80%A6]GFsIn0.f0656mzmcRMSCywkbEptdd6JgkDfIqN0S9t-P1aPyt8&id=8158"
-        val baseUrlOfMagicLink =
-            "https://star-health.getvisitapp.xyz/" //the need of baseUrl if only for updating the google fit daily steps card after the webpage as loaded.
 
 
-        IntiateSdk.s(this, false, magicLink, baseUrlOfMagicLink, default_client_id)
+        IntiateSdk.s(this, false, prodLinkMagicLink, prodBaseUrl, default_client_id)
     }
 }
