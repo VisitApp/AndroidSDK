b03ce57ba491c18ce4d7050d962ce74b0c00fb8d
Chinmay <chinmaydash11@gmail.com>
2022-02-04 14:56:10 +0530

    added comments

--- google_fit/src/main/java/com/getvisitapp/google_fit/event/VisitEventType.kt
+++ google_fit/src/main/java/com/getvisitapp/google_fit/event/VisitEventType.kt
@@ -1,8 +1,25 @@
 package com.getvisitapp.google_fit.event
 
 sealed class VisitEventType {
+    /**
+     * Called when user clicked on Connect to Google Fit
+     */
     object AskForFitnessPermission : VisitEventType()
+
+    /**
+     * Called after the user has successfully connected to Google Fit
+     */
     object FitnessPermissionGranted : VisitEventType()
+
+
+    /**
+     * Called when the PWA request for health data for certain type.
+     * @param type can be 'day' or 'month' or 'week'
+     * @param frequency can be 'steps' or 'distance' or 'calories' or 'sleep'
+     * @param timestamp is a type epoxy timestamp
+     */
+
+
     class RequestHealthDataForDetailedGraph(
         var type: String?,
         var frequency: String?,
@@ -10,7 +27,14 @@ sealed class VisitEventType {
     ) :
         VisitEventType()
 
+    /**
+     * Called when the PWA request for Location permission
+     */
     object AskForLocationPermission : VisitEventType()
+
+    /**
+     * Called when the PWA request for video call.
+     */
     class StartVideoCall(var sessionId: Int, var consultationId: Int, var authToken: String?) :
         VisitEventType()
 
