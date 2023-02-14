7370321bf9dc48adf945a397fdf22f538876e4d1
chinmay <chinmaydash11@gmail.com>
2023-02-14 11:48:52 +0530

    removed unwanted fitness scopes.

--- google_fit/src/main/java/com/getvisitapp/google_fit/FitnessPermissionUtil.java
+++ google_fit/src/main/java/com/getvisitapp/google_fit/FitnessPermissionUtil.java
@@ -72,24 +72,13 @@ public class FitnessPermissionUtil {
 
     private void askFitnessPermissions(GoogleSignInAccount googleSignInAccount, boolean isLastSignedIn) {
         if (googleSignInAccount != null) {
-            fitnessOptions = FitnessOptions.builder()
-                    .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE, FitnessOptions.ACCESS_WRITE)
+            fitnessOptions =  FitnessOptions.builder()
                     .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE, FitnessOptions.ACCESS_READ)
 
-                    .addDataType(DataType.TYPE_ACTIVITY_SEGMENT, FitnessOptions.ACCESS_WRITE)
                     .addDataType(DataType.TYPE_ACTIVITY_SEGMENT, FitnessOptions.ACCESS_READ)
 
                     .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                    .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_WRITE)
 
-                    .addDataType(DataType.TYPE_WEIGHT, FitnessOptions.ACCESS_READ)
-                    .addDataType(DataType.TYPE_WEIGHT, FitnessOptions.ACCESS_WRITE)
-
-                    .addDataType(DataType.TYPE_HEIGHT, FitnessOptions.ACCESS_READ)
-                    .addDataType(DataType.TYPE_HEIGHT, FitnessOptions.ACCESS_WRITE)
-
-                    .addDataType(DataType.TYPE_LOCATION_SAMPLE, FitnessOptions.ACCESS_READ)
-                    .addDataType(DataType.TYPE_LOCATION_SAMPLE, FitnessOptions.ACCESS_WRITE)
 
                     .addDataType(DataType.TYPE_DISTANCE_DELTA)
                     .addDataType(DataType.AGGREGATE_DISTANCE_DELTA)
