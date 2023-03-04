34fa64910244dcfd1740bcd057a3daf4d3f688a9
chinmay <chinmaydash11@gmail.com>
2023-03-04 12:13:09 +0530

    removed unwanted scopes.

--- google_fit/src/main/java/com/getvisitapp/google_fit/FitnessPermissionUtil.java
+++ google_fit/src/main/java/com/getvisitapp/google_fit/FitnessPermissionUtil.java
@@ -50,9 +50,7 @@ public class FitnessPermissionUtil {
                 .requestScopes(new Scope(Scopes.EMAIL),
                         new Scope(Scopes.PROFILE),
                         new Scope(Scopes.PLUS_ME),
-                        new Scope(Scopes.FITNESS_ACTIVITY_READ),
-                        new Scope(Scopes.FITNESS_LOCATION_READ),
-                        new Scope(Scopes.FITNESS_BODY_READ))
+                        new Scope(Scopes.FITNESS_ACTIVITY_READ))
                 .requestServerAuthCode(defaultWebClientId, false)
                 .requestIdToken(defaultWebClientId)
                 .build();
