c2f93b37949ee522cbd0029f3c6b15a5d6386e37
chinmay <chinmaydash11@gmail.com>
2024-09-02 08:17:02 +0530

    added health connect code.

---
+++ google_fit/src/main/java/com/getvisitapp/google_fit/HealthConnectListener.kt
@@ -0,0 +1,11 @@
+package com.getvisitapp.google_fit
+
+import com.getvisitapp.google_fit.healthConnect.enums.HealthConnectConnectionState
+
+
+interface HealthConnectListener {
+    fun updateHealthConnectConnectionStatus(status: HealthConnectConnectionState, text: String)
+
+    //This callback is used for both dashboard graph and detailed graph.
+    fun loadVisitWebViewGraphData(webUrl: String)
+}
