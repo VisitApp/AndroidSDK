65609c9a08f766eddb05282fd00c5841f2f77c11
chinmay <chinmaydash11@gmail.com>
2024-09-03 20:39:28 +0530

    changed min sdk version to 31.

---
+++ google_fit/src/main/java/com/getvisitapp/google_fit/healthConnect/activity/ViewPermissionUsageActivity.kt
@@ -0,0 +1,11 @@
+package com.getvisitapp.google_fit.healthConnect.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ViewPermissionUsageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
