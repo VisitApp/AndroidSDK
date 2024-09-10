e4b33c1682c90f9f9003b746dfb1ca6e68676403
chinmay <chinmaydash11@gmail.com>
2024-09-10 13:27:59 +0530

    disabled buildConfig and remove buildConfig fields.

--- google_fit/src/main/java/com/getvisitapp/google_fit/healthConnect/activity/ViewPermissionUsageActivity.kt
+++ google_fit/src/main/java/com/getvisitapp/google_fit/healthConnect/activity/ViewPermissionUsageActivity.kt
@@ -1,7 +1,6 @@
 //package com.getvisitapp.google_fit.healthConnect.activity
 //
 //import android.os.Bundle
-//import androidx.activity.enableEdgeToEdge
 //import androidx.appcompat.app.AppCompatActivity
 //import com.getvisitapp.google_fit.R
 //
