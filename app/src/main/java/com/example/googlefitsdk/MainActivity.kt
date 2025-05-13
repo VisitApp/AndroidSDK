a779439e831a60254267183ee4a8a7d84003063c
chinmay <39885077+chinmaydash007@users.noreply.github.com>
2025-05-13 09:35:46 +0530

    made enter url dynamic.

--- app/src/main/java/com/example/googlefitsdk/MainActivity.kt
+++ app/src/main/java/com/example/googlefitsdk/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.googlefitsdk
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.getvisitapp.google_fit.IntiateSdk
 
@@ -11,7 +12,6 @@ class MainActivity : AppCompatActivity() {
 
     private val default_client_id =
         "74319562719-7rart63dq265045vtanlni9m8o41tn7o.apps.googleusercontent.com"
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,14 +25,13 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     fun init() {
 
 //        val magicLink =
 //            "https://web.getvisitapp.xyz/"
 
-        val magicLink =
-            "https://abhi.getvisitapp.net/sso?userParams=t1Q8E-aBzmU2_Y7xi17gjrMzTGsvH32N9GeePukIc-KjG6bmMrfsLpmrzpqNiKFkgfSB3NMF0SPeQMXITQ6QXBLpUzc7fLrgsdyF8MOw46_02_YH7ogZH_oekVYByGaQ-qSIyP3P8aCxGq5tIZ7QRxImqGtQxeV4pJXJCddGihL7-eIbe5ivM-cNQMM3iHkoNGR7ximmaCOK6iXVQVfxLMyuSFL5O7VCild5iphHX1s&clientId=abhi-58fd14"
+//        val magicLink =
+//            "https://abhi-sdk.getvisitapp.com/sso?userParams=8d6wbkkDSHcf1oQG3GPuE1a1GQ4VDObte-tpi3wI1xVwT2mOttZ2cWY1fSV86FvPz_axlxlhx0lZD2PoE3xpzUBEnfNSN0NAc6AwsukBSJQAhRYJxxTjrBFy6re-PfqOgJLEetNtj4EGguWuqgpHjeaOlmss1TkAGSBlTfgFYOd32IzzDYItZXwpv9BNOjBVhQQn2fhUkyOvaXmg9WsIxQ_8EmzaVwaMw0s_J_GHCFOu2lN8ea9j3xBC9SbEWKDZwXNnd63xHU8GQ4luzVrqNx8IGkUz6bS-FbR_9czjQl-MT2icWSUa8_hQSp41hobn&clientId=abhi-58fd14"
 
 
 //        val magicLink =
@@ -42,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         IntiateSdk.s(
             this,
             false,
-            magicLink,
+            findViewById<EditText>(R.id.editTextText).text.toString(),
             default_client_id
         )
     }
